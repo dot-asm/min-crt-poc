0bf6996ea6d9c45be6e6fdeebc4cf6e4f101960d
Andy Polyakov <appro@cryptogams.org>
2022-04-08 21:00:31 +0200

    build.rs: refuse to compile with msvc.

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    if std::env::var("CARGO_CFG_WINDOWS").is_err() {
-        panic!("this is a Windows-only crate");
+    if std::env::var("CARGO_CFG_WINDOWS").is_err()
+        || std::env::var("CARGO_CFG_TARGET_ENV").unwrap().eq("msvc")
+    {
+        panic!("this is a MinGW-only crate");
     }
     println!("cargo:rustc-cdylib-link-arg=-nostdlib");
     println!("cargo:rustc-cdylib-link-arg=-Wl,--entry=DllMain");
